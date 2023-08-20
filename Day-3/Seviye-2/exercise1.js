87d3be7f3563c719037922f291527029888a1e92
Anıl Sezgin <131723917+dev-anko@users.noreply.github.com>
2023-08-20 16:35:10 +0300

    Add files via upload

---
+++ Day-3/Seviye-2/exercise1.js
@@ -0,0 +1,18 @@
+// Bu kodların çalışması için bir html dosyası veya google dev tools kullanmanız gerekmektedir.
+let a = prompt("Yükseklik:");
+let b = prompt("Taban");
+let c = a*b/2
+alert("Alan: "+c);
+
+/*-----------------------------------------------*/
+let d = Number(prompt("a kenarı:"));
+let e = Number(prompt("b kenarı:"));
+let f = Number(prompt("c kenarı:"));
+let cevre = a+b+c;
+alert(cevre);
+
+/*----------------------------------------------*/
+
+// 3,4,5,6,7 ve 9nin 1den farkı olmadığı için yapmıyorum.
+
+// 8'in birden fazla çözüm yöntemi var ve yapmayı gereksiz gördüm.
---
+++ Day-3/Seviye-2/exercise10-11.js
@@ -0,0 +1,15 @@
+let name1 = prompt("Adınız nedir?");
+name.length>7
+    ? alert("your name is longer than 7")
+    : alert ("your name is shorter than 7")
+
+/*------------------------------------------*/
+
+let name2 = prompt("Adınız nedir?");
+let surname = prompt("Soyadınız nedir?")
+name.length>surname.length
+    ? alert("your name is longer than your surname")
+    : alert ("your name is shorter than your surname")
+
+// 12 gereksiz.
+
